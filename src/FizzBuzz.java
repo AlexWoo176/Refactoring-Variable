cf1a0bd0e0b2417c7ebdd165f746a612e2ce657b
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-03-30 12:07:02 +0700

    Complete

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,12 +1,15 @@
 public class FizzBuzz {
+
     public static String fizzBuzz(int number){
-        if(number % 3 == 0 && number % 5 == 0)
+        boolean isDivisibleBy3 = number % 3 == 0;
+        boolean isDivisibleBy5 = number % 5 == 0;
+        if(isDivisibleBy3 && isDivisibleBy5)
             return "FizzBuzz";
 
-        if(number % 3 == 0)
+        if(isDivisibleBy3)
             return "Fizz";
 
-        if(number % 5 == 0)
+        if(isDivisibleBy5)
             return "Buzz";
 
         return number + "";
